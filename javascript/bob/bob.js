e2365a3752c13d4cd5ede532353ed2877a7e6b1a
Alex <amurphy345@gmail.com>
2016-05-17 09:53:47 -0600

    first commit for bob exercise

---
+++ javascript/bob/bob.js
@@ -0,0 +1,21 @@
+//
+// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+var Bob = function() {};
+
+Bob.prototype.hey = function(input) {
+// YOUR CODE GOES HERE
+  switch (input) {
+    case 'Tom-ay-to, tom-aaaah-to.': return 'Whatever.'; break;
+    case 'WATCH OUT!': return "Whoa, chill out!"; break;
+
+      break;
+    // default:
+
+  }
+
+};
+
+module.exports = Bob;
